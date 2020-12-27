7adde49a6c3c1b8f620213cb5ff95c11de35fc3f
gaowei1012 <mingwei2222@126.com>
2020-12-27 10:08:50 +0800

    fix user sign out

--- elm_server/src/router/user.js
+++ elm_server/src/router/user.js
@@ -3,6 +3,6 @@ const controller = require('../controller/user')
 
 router.post('/api/user/register', controller.register)
 router.post('/api/user/login', controller.login)
-router.post('/api/user/sign_out', controller.signOut)
+router.get('/api/user/sign_out', controller.signOut)
 
 module.exports = router
