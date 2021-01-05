2010ca2dbe136c2075070a28254e939f24a57c89
gaowei1012 <mingwei2222@126.com>
2021-01-05 19:46:22 +0800

    fix cors

--- elm_server/src/app.js
+++ elm_server/src/app.js
@@ -14,7 +14,19 @@ const sessionMysqlConfig = {
     database: database.DATABASE,
     host: database.HOST,
 }
-app.use(cors())
+app.use(cors({
+    origin: function(ctx) { //设置允许来自指定域名请求
+        if (ctx.url === '/') {
+            return '*'; // 允许来自所有域名请求
+        }
+        return 'http://localhost:1230';
+    },
+    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    credentials: true, //是否允许发送Cookie
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+}))
 app.use(
     session({
         key: 'USER_SID',
